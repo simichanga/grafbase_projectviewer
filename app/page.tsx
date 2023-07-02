ef12dccea0edc64926ca7772c10cf041694e0d85
EpicShadowXD <siminischi1234@gmail.com>
2023-07-02 15:39:23 +0300

    upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section className="flex-start flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>LoadMore</h1>
+        </section>
+    )
+}
+
+export default Home;
